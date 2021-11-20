3da1b54fcdaaef76191d95d07559e766b59edc73
chernyaevpavel <pawliklist@yandex.ru>
2021-11-20 22:33:20 +0400

    Create Project

---
+++ BreakingBadSwiftUICombine/BreakingBadSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadSwiftUICombineApp.swift
+//  BreakingBadSwiftUICombine
+//
+//  Created by Павел Черняев on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
